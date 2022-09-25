51dfaabe6716c8fd17077e32af301243038eaaf6
ssamkyu <ssamkyu@hanmail.net>
2022-09-25 15:47:32 +0900

    update again

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const config = {
     classNames: [], // List of classes to support for query subscriptions
   },
 };
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
